fd399f8d3227d8987e63b13b9893fdab533100fb
franka08 <168740945+franka08@users.noreply.github.com>
2025-05-15 19:25:38 +0200

    Add file to test pose recognition without unity

---
+++ pose_detection_alone.py
@@ -0,0 +1,106 @@
+# ---------------------- Imports ---------------------- #
+import os
+import sys
+import cv2
+import numpy as np
+import joblib
+
+import socket
+
+import tensorflow as tf
+from sklearn.preprocessing import MinMaxScaler
+
+# ---------------------- Global Settings ---------------------- #
+
+HOST = '127.0.0.1'  # Localhost
+PORT = 65432        # Port to send data to
+
+# ---------------------- Mediapipe Setup ---------------------- #
+import mediapipe as mp
+from mediapipe.framework.formats import landmark_pb2
+
+mp_pose = mp.solutions.pose
+pose = mp_pose.Pose(static_image_mode=True)
+mp_drawing = mp.solutions.drawing_utils
+
+# ---------------------- Functions ---------------------- #
+
+def extract_keypoints(image, pose):
+    results = pose.process(image)
+    keypoints = []
+
+    if results.pose_landmarks:
+        for lm in results.pose_landmarks.landmark:
+            keypoints.extend([lm.x, lm.y, lm.z, lm.visibility])
+
+    return keypoints
+
+def detect_pose(photo, model, scaler, pose):
+    keypoints_array = extract_keypoints(photo, pose)
+    if len(keypoints_array) == 33 * 4:
+        # Preprocess new data before predicting
+        X_new = scaler.transform([keypoints_array])
+        predictions = model.predict(X_new)
+        confidence = np.max(predictions)
+        # safety threshold that no pose is sent if model is very unsure
+        if confidence < 0.6:
+            return 404
+        else:
+            predicted_pose = np.argmax(predictions, axis=1).item()
+            return predicted_pose
+    else:
+        return 404 # no pose detected because keypoints where not extracted correctly
+
+def send_pose_number(num, client):
+    try:
+        message = f"{num}\n"
+        client.sendall(message.encode('utf-8'))
+    except Exception as e:
+        print("Error sending pose number:", e)
+
+def resource_path(relative_path):
+    try:
+        # When using PyInstaller
+        base_path = sys._MEIPASS
+    except AttributeError:
+        # When running normally
+        base_path = os.path.abspath(".")
+    return os.path.join(base_path, relative_path)
+
+
+# ---------------------- Main Script ---------------------- #
+if __name__ == "__main__":    
+    save_dir = resource_path('TrainedModels')
+    scaler = joblib.load(os.path.join(save_dir, 'scaler.pkl'))
+    mlp_model = tf.keras.models.load_model(os.path.join(save_dir, 'pose_classifier_model.h5'))
+
+    # set up connection to game
+    # client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # client.connect((HOST, PORT))
+
+    # open camera
+    cap = cv2.VideoCapture(0)
+
+    # receive variables from game that game is started
+    # maybe substitute and start script when game is startet??
+    in_game = True
+
+    while (in_game):
+        success, frame = cap.read()
+        if not success:
+            break
+        mirrored_frame = cv2.flip(frame, 1) # mirrow image as mlp was trained with camera images
+        cv2.imshow('frame', mirrored_frame)
+        pose_num = detect_pose(frame, mlp_model, scaler, pose)
+        print(f"Detected pose: {pose_num}")
+
+        if (pose_num <= 12 and pose_num >= 0):
+            # send_pose_number(pose_num, client)
+            pass
+
+        if cv2.waitKey(5) & 0xFF == ord('q'):  # Q to quit
+            break
+
+    cap.release()
+    # client.close()
+    cv2.destroyAllWindows()
